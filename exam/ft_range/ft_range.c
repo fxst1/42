eacecedad89e97fde6d17d8421477459e108e992
fxst1 <faustin.jacquemain@hotmail.fr>
2018-07-16 16:41:33 +0200

    Update ft_range.c

--- exam/ft_range/ft_range.c
+++ exam/ft_range/ft_range.c
@@ -4,7 +4,7 @@ int *ft_range(int start, int end)
   int *ptr = NULL;
   int i = 1;
   int size = (end - start > 0) ? (end - start) : (start - end);
-  if ((ptr = (int*)malloc(sizeof(int) * 1) ))
+  if ((ptr = (int*)malloc(sizeof(int) * (size + 1)) ))
   {
     ptr[0] = start;
     while (i <= size)
